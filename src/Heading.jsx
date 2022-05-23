c18714cbf2dcc2a7ca90692908387faa652b5843
Zohaib R <zohaibr123@hotmail.com>
2022-05-23 07:37:51 -0400

    First look at react components

---
+++ src/Heading.jsx
@@ -0,0 +1,8 @@
+import react from "react"
+
+function Heading(){
+ 
+    return <h1>My favorite foods</h1>
+
+}
+export default Heading; 
---
+++ src/List.jsx
@@ -0,0 +1,14 @@
+import react from "react"
+
+function List(){
+
+    return(<ul>
+        <li>Shawarma </li>
+        <li>Pizza</li>
+        <li>Lasagne</li>
+    </ul>
+
+    );
+}
+
+export default List;
